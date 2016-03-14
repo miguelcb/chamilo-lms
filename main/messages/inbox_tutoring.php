3795104cfbd5fc66b2ba33ec50f60276de54991e
danilobrinu <ddbn.c2@gmail.com>
2016-03-14 11:22:10 -0500

    Minor changes - messages(alumn)

--- main/messages/inbox_tutoring.php
+++ main/messages/inbox_tutoring.php
@@ -31,7 +31,7 @@ $result = Database::query($sql);
             <li class="list-group-item messages-tutoring__item" role="button" data-message-id="<?php echo $row['id']; ?>">
                 <h4 class="list-group-item-heading clearfix" style="position: relative; padding-right: 24px;">
                     <div class="pull-left"><?php echo $row['user_sender']; ?></div>
-                    <div class="pull-right small" style="padding: 0;"><?php echo api_convert_and_format_date($row['sender_date'], '%b %d') ?></div>
+                    <div class="pull-right small" style="padding: 0;"><?php echo api_convert_and_format_date($row['send_date'], '%b %d') ?></div>
                     <div class="btn-group" style="position: absolute; right: 0;">
                       <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="padding: 2px 4px; line-height: 1;">
                         <span class="caret"></span>
@@ -39,7 +39,6 @@ $result = Database::query($sql);
                       </button>
                       <ul class="dropdown-menu dropdown-menu-right">
                         <li>
-                            <!--<a class="message-tutoring__item__delete" onclick="javascript:if(!confirm('<?php echo addslashes(api_htmlentities(get_lang('ConfirmDeleteMessage'))); ?>')) return false;" href="<?php echo api_get_path(WEB_CODE_PATH); ?>messages/inbox.php?action=deleteone&id=<?php echo $row['id'] ?>"><?php echo get_lang('DeleteMessage'); ?></a>-->
                             <a class="message-tutoring__item__delete" href="#" data-message-delete-id="<?php echo $row['id']; ?>"><?php echo get_lang('DeleteMessage'); ?></a>
                         </li>
                       </ul>
